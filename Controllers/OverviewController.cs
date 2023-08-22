ad3d35e0771fdbce9d8f0e5e01d6638f5620d3e2
Michal Wilkosz <mw7883475@gmail.com>
2023-08-22 14:58:27 +0200

    wyszukiwanie

---
+++ Controllers/OverviewController.cs
@@ -0,0 +1,25 @@
+﻿using Communicator.Data;
+using Communicator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Communicator.Controllers
+{
+    public class OverviewController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public OverviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+
+        public IActionResult Search(string searchingText)
+        {
+            OverviewPageViewModel overviewPageViewModel = new OverviewPageViewModel();
+            List<ApplicationUser> users = _context.Users.Where(x => x.Name.Contains(searchingText) || x.LastName.Contains(searchingText) || x.Nick.Contains(searchingText)).ToList();
+            overviewPageViewModel.Users = users;
+
+            return View("Index", overviewPageViewModel);
+        }
+    }
+}
---
+++ Models/OverviewPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Communicator.Models
+{
+    public class OverviewPageViewModel
+    {
+        public List<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+
+    }
+}
