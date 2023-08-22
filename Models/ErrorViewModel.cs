c31cc98bdda5b51c6935147e7d4f356e0bd4fc15
Kamil Surlas <ks303231@student.polsl.pl>
2023-08-22 10:19:41 +0200

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Communicator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
