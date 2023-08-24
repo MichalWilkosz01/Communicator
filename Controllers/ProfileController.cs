18f93d1e946dac8afac19e673015f5877ac240e2
Kamil Surlas <ks303231@student.polsl.pl>
2023-08-24 13:46:32 +0200

    edit

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,6 +1,49 @@
-﻿namespace Communicator.Controllers
+﻿using Communicator.Data;
+using Communicator.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
+
+namespace Communicator.Controllers
 {
-    public class ProfileController
+    public class ProfileController:Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+       
+        public ProfileController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _context = context;
+            
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> EditUser()
+        {
+            
+            var id = _userManager.GetUserId(this.User);
+            var user = _userManager.Users.FirstOrDefault(u => u.Id == id);
+
+            var editModel = new EditUserViewModel
+                {
+                    Id = user.Id,
+                    Nick = user.Nick,
+                    LastName = user.LastName,
+                    City = user.City,
+                    Country = user.Country,
+                    PhoneNumber = user.PhoneNumber,
+                    Age = user.Age
+
+                };
+
+            Debug.WriteLine(editModel.Id);
+            return View(editModel);
+        }
+        
     }
 }
---
+++ Models/EditUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Communicator.Models
+{
+    public class EditUserViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]     
+        public string Nick { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
